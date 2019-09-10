dfbc2c2f86c37733e6b60c5abba4f4d37af57fcd
Parth Oberoi <htrapdev@gmail.com>
2019-09-10 17:26:41 +0530

    update block explorers

--- lib/util.py
+++ lib/util.py
@@ -506,15 +506,15 @@ def time_difference(distance_in_time, include_seconds):
         return "over %d years" % (round(distance_in_minutes / 525600))
 
 mainnet_block_explorers = {
-    'zclele.duckdns.org:3001': ('http://zclele.duckdns.org:3001',
-                        {'tx': 'tx', 'addr': 'address'}),
-    'zclassic-ce.io': ('http://zclassic-ce.io',
-                        {'tx': 'tx', 'addr': 'address'}),
+    'zeltrez.io': ('https://explorer.zcl.zeltrez.io/',
+                        {'tx': 'tx/', 'addr': 'address/'}),
+    'zclassic-ce.org': ('http://explorer.zclassic-ce.org/',
+                        {'tx': 'tx/', 'addr': 'address/'}),
 }
 
 testnet_block_explorers = {
-    'testnet.z.cash': ('https://explorer.testnet.z.cash/',
-                       {'tx': 'tx/', 'addr': 'address/'}),
+    # 'testnet.z.cash': ('https://explorer.testnet.z.cash/',
+    #                    {'tx': 'tx/', 'addr': 'address/'}),
     'system default': ('blockchain:/',
                        {'tx': 'tx/', 'addr': 'address/'}),
 }
@@ -524,7 +524,7 @@ def block_explorer_info():
     return testnet_block_explorers if constants.net.TESTNET else mainnet_block_explorers
 
 def block_explorer(config):
-    return config.get('block_explorer', 'zclassic-ce.io')
+    return config.get('block_explorer', 'zeltrez.io')
 
 def block_explorer_tuple(config):
     return block_explorer_info().get(block_explorer(config))
